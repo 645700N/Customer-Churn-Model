8d186e82556a7bb7cff268b1ad1a951d55166c5f
700N645 <700n645@gmail.com>
2025-01-31 18:51:35 -0300

    Agregacion de columnas totales y grafico de barras con respecto a estados y total de llamdas

--- customer_churn_model.py
+++ customer_churn_model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('dataset/Customer Churn Model.txt')
 
@@ -12,4 +13,38 @@ columnas_español = ['Estado', 'longitud de la cuenta', 'Codigo de area', 'Telef
 
 data2 = pd.read_csv('dataset/Customer Churn Model.txt', header=0, names=columnas_español)
 
-print(data2.head())
+
+#Filtrado de personas con el plan internacional (type=df)
+df_intl_plan = data[data["Int'l Plan"] == 'yes']
+#Columna de personas con el plan internacional (type=series)
+intl_plan = df_intl_plan["Int'l Plan"]
+#Columna de minutos internacionael de personas que poseen el plan internacional (type=series)
+intl_mins = df_intl_plan["Intl Mins"]
+
+#filtrado por iloc(basado en posicion) y loc(basado en etiqueta)
+sample_data = data.iloc[:20, 3:6]
+sample_data1 = data.iloc[:20, [2,4,6]]
+
+sample_data2 = data.loc[:20, ['State', 'Day Mins']]
+
+#Estados unicos en el dataframe (states=51)
+states = data['State'].drop_duplicates()
+    
+
+data['Total Mins'] = (data['Day Mins'] + data['Night Mins'] + data['Eve Mins'])
+data['Total Calls'] = (data['Day Calls'] + data['Night Calls'] + data['Eve Calls'])
+data['Total Charge'] = (data['Day Charge'] + data['Night Charge'] + data['Eve Charge'])
+
+#Recorrera la lista de estados y sumara el total de llamadas por estado, almacenando los valores en una lista
+state_list = []
+for x in states:
+    state = sum(data[data['State'] == x].iloc[:, 22])
+    state_list.append(state)
+
+states = states.tolist()
+total_state_calls = pd.DataFrame(state_list, states)
+print(total_state_calls)
+
+plt.bar(states, state_list)
+plt.xticks(rotation=60, horizontalalignment='left')
+plt.show()
